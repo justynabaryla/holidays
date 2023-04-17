8ab65865c13b9a6a7fb568b9c6a2fd212517ed77
justynabaryla <justyna.baryla@yahoo.co.uk>
2023-04-17 23:07:26 +0100

    header component built

---
+++ src/Title.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Title() {
+  return (
+   
+          <div className="Title">
+            <h1>This is a header</h1>
+            
+            <div className="Title-Subtitle">this would be additional text</div>
+                
+                  
+           
+         </div>
+        )
+      }
+      
+      export default Title
